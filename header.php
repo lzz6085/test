82d74be7088e8d66e67f4468ea2b301d8a05936a
lzz6085 <lzz6085@gmail.com>
2013-03-27 14:53:18 +0800

    add all

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Website Name Here</title>
+<meta charset="GBK">
+<!--[if IE]><![endif]-->
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="css/style.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script>
+<script src="js/scripts.js"></script>
+<!--[if lt IE 9]>
+<script src="http://css3-mediaqueries-js.googlecode.com/files/css3-mediaqueries.js"></script>  
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+
+<div id="fixedtop" class="clearfix">
+  
+	<div id="headercont" class="clearfix">
+       <a id="headerleft" title="" href="#">MovieTracker<br/><font color="black">MTer.org</font></a>
+    	<div id="headerright">
+            <div class="menu">
+                <ul>
+                    <li class="active"><a title="" href="#">热门电影</a></li>
+                   <li><a title="" href="#">最近更新</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="topheight"></div>
+
+<div id="maincont" class="clearfix">
+    <div id="main">
---
+++ nav.php
@@ -0,0 +1,95 @@
+<nav class="nav"> 
+<ul>
+<li>类型</li>
+<li class="current">
+<a href="#">不限</a>
+</li>
+
+<li >
+<a href="#">喜剧</a>
+</li>
+
+<li >
+<a href="#">爱情</a>
+</li>
+<li >
+<a href="#">喜剧</a>
+</li>
+
+<li >
+<a href="#">爱情</a>
+</li>
+<li >
+<a href="#">喜剧</a>
+</li>
+
+<li >
+<a href="#">爱情</a>
+</li>
+<li >
+<a href="#">喜剧</a>
+</li>
+
+<li >
+<a href="#">爱情</a>
+</li>
+<li >
+<a href="#">喜剧</a>
+</li>
+
+<li >
+<a href="#">爱情</a>
+</li>
+<li >
+<a href="#">喜剧</a>
+</li>
+
+<li >
+<a href="#">爱情</a>
+</li>
+<li >
+<a href="#">喜剧</a>
+</li>
+
+<li >
+<a href="#">爱情</a>
+</li>
+</ul>
+</nav> 
+
+
+<nav class="nav"> 
+<ul>
+<li>类型</li>
+<li class="current">
+<a href="#">不限</a>
+</li>
+
+<li >
+<a href="#">喜剧</a>
+</li>
+
+<li class="current">
+<a href="#">爱情</a>
+</li>
+</ul>
+</nav> 
+
+<nav class="nav"> 
+<ul>
+<li>类型</li>
+<li >
+<a href="#">不限</a>
+</li>
+
+<li class="current">
+<a href="#">喜剧</a>
+</li>
+
+<li >
+<a href="#">爱情</a>
+</li>
+</ul>
+</nav> 
+
+<hr />
