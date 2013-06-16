637c2520da9b8420a284eb8929e57a55bf8b3bbd
lzz6085 <lzz6085@gmail.com>
2013-06-16 18:50:17 +0800

    fix pic

--- images.php
+++ images.php
@@ -4,10 +4,14 @@ $id = isset($_REQUEST['id']) && $_REQUEST['id'] > 0 ? intval($_REQUEST['id']) : 
 
 $sql = "select pic from mt.dm where id = $id";
 
-$ret = mysqli_prepared_query($sql);
-if (empty($ret[0]['pic'])) {
+//$ret = mysqli_prepared_query($sql);
+$conn = mysqli_connect("127.0.0.1", "root");
+$result = $conn->query($sql);
+$ret = null;
+$ret = $result->fetch_row();
+if (empty($ret[0])) {
 	header('Location: ./images/portfolio-1t.jpg');
 } else {
 	header('Content-type: image/jpeg');
-	echo isset($ret[0]['pic']) ? $ret[0]['pic'] : '';
+	echo isset($ret[0]) ? $ret[0] : '';
 }
